e90721fc4f61f4e651ec02a0cba2fe3df9e70a17
rtoddm <101154587+rtoddm@users.noreply.github.com>
2022-03-31 22:37:26 +0300

    Add files via upload

---
+++ JavaDayOneHW/src/JavaHW2.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class JavaHW2 {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("How old are you?");
+		String yourage = input.nextline();
+		System.out.println("You are %s years old!");
+		
+		//The above code doesn't work for several reasons.  Camelcasing is missing, printing with
+		//"println" instead of "printf", the "L" in nextLine should be capitalized. I also forgot the 
+		//yourAge variable on line 11. 
+		//Below these mistakes have been fixed.  
+		
+		//System.out.println("How old are you?");
+		//String yourAge = input.nextLine();
+		//System.out.printf("You are %s years old!", yourAge);
+		
+		int x;
+		System.out.println(x);
+		
+		//The above code states that x may not have been initialized.  
+		
+		
+		
+		}
+		
+	}
+
+
---
+++ JavaDayOneHW/src/JavaTypesVariablesBranching.java
@@ -0,0 +1,36 @@
+
+
+import java.util.Scanner;
+
+public class JavaTypesVariablesBranching {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Hello, World!");
+		
+		//The first if statement produces an error because firstName hasn't been defined yet while the
+		//second if statement works because firstName is inside it's scope and has been defined. 
+		
+		if(firstName.equals("Bill")) {
+			System.out.println("You're not Todd!");
+		
+		//System.out.println("What is your name?");
+		//String firstName = input.nextLine();
+		//System.out.printf("Hello %s!", firstName);
+		
+		//if(firstName.equals("Todd")) {
+		//	System.out.println("That's my name too!");
+		//}else {
+		//	System.out.println("That's a cool name!");
+		}
+		
+		// Your First Program
+
+		
+		
+		}
+		
+	}
+	
+
